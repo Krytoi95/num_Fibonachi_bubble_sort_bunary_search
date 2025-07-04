53048e3cb4e20aabab6b4ec092b8787a02b5554a
Krytoi95 <krytoiskelet95@gmail.com>
2025-07-04 19:21:38 +0200

    Place in Number, Number in Place, User Fibonachi

--- num_Fibonachi_bubble_sort_bunary_search/num_Fibonachi_bubble_sort_bunary_search.cpp
+++ num_Fibonachi_bubble_sort_bunary_search/num_Fibonachi_bubble_sort_bunary_search.cpp
@@ -1,36 +1,129 @@
 #include <iostream>
 
-//Number Fibonachi
+//Numbers Fibonachi
 
-int Numbers_Fibonachi(int &place,const int number_stop = 233, int number1 = 0, int number2 = 1)
-{ 
-	int buffer{ number1 + number2 };
-	place++;
-	if (number_stop == buffer)
-	{
-		return place;
-	}
-	if (number_stop == 0)
-	{
-		return 1;
-	}
-	if (number_stop == 1)
-	{
-		return 1;
-	}
-	if (buffer>number_stop)
+// place in number 
+int Place_number_Fibonachi(int place_U,int stop_func = 0,int number1 = 0, int number2 = 1)
+{
+	// place user
+	if (place_U == 1)
 	{
 		return 0;
 	}
+	if(place_U == 2||place_U==3)
+	{
+		return 1;
+	}
+	int buffer{ number1 + number2 };
+	stop_func++;
+	if(stop_func>place_U)
+	{ 
+		return -1;
+	}
+	if (stop_func == place_U)
+	{
+		return buffer;
+	}
 	number1 = number2;
-	return Numbers_Fibonachi(place, number_stop,number1,buffer);
+	return Place_number_Fibonachi(place_U,stop_func,number1,buffer);
 }
-// Человек вписывает значение и нужно определить, есть ли это значение среди числе Фибоначчи. Если есть, вернуть на каком месте. Если нету, вернуть ошибку.
-
+// number in place 
+int Number_Fibonachi(int& place_U, const int number_stop_func=0, int number1 = 0, int number2 = 1)
+{
+	if(number_stop_func==0)
+	{
+		place_U = 1;
+		return place_U;
+	}
+	if(number_stop_func==1)
+	{
+		place_U = 2;
+		return place_U;
+	}
+	int buffer{ number1 + number2 };
+	place_U++;
+	if (number_stop_func == buffer)
+	{
+		return place_U;
+	}
+	if (buffer > number_stop_func)
+	{
+		return -1;
+	}
+	number1 = number2;
+	return Number_Fibonachi(place_U, number_stop_func, number1, buffer);
+}
+// User Fibonachi
+int User_Number_Fibonachi( int number_user1, int number_user2,int stop_func = 0)
+{
+	int buffer = { number_user1 + number_user2 };
+	if (stop_func == 0)
+	{
+		return buffer;
+	}
+	number_user1 = number_user2;
+	stop_func--;
+	return User_Number_Fibonachi(number_user1, buffer,stop_func);
+}
 int main()
 {
-	int place{ 2 };
-	std::cout << Numbers_Fibonachi(place);
+	int place{ 0 };
+	int number_user{0};
+	int number{0};
+	std::cout << "Enter number: " << std::endl;
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << User_Number_Fibonachi(180, 115,i) << std::endl;
+	}
+
+
+
+
+
+
+
+
+
+
+	
+	/*while(std::cin>>number_user)
+	{
+		if (number_user != 0 || number_user != 1)
+		{
+			place += 2;
+		}
+		number_user = Number_Fibonachi(place, number_user);
+		if (number_user == 0)
+		{
+			std::cout << "This number isn't range Number_Fibonachi" << std::endl;
+			place = 0;
+		}
+		else if (number_user == 2)
+		{
+			std::cout << "Place: 2-3" << std::endl;
+			place = 0;
+		}
+		else
+		{
+			std::cout << "Place: " << number_user << std::endl;
+			place = 0;
+		}
+	}*/
+
+	/*while (std::cin >> place)
+	{
+		number = Place_number_Fibonachi(place);
+		if(number==-1)
+		{
+			std::cout << "Error" << std::endl;
+		}
+		else 
+		{ 
+			std::cout << "Number: " << Place_number_Fibonachi(place) << std::endl; 
+		}
+	}*/
+	
+	std::cout << "Bye!" << std::endl;
 	return 0;
 }
 

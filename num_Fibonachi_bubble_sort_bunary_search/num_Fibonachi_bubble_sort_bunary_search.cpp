0c9403e85b2cebf133ffc199ef45a2b73a7441e0
Krytoi95 <krytoiskelet95@gmail.com>
2025-07-05 12:45:55 +0200

    Dynamic array

--- num_Fibonachi_bubble_sort_bunary_search/num_Fibonachi_bubble_sort_bunary_search.cpp
+++ num_Fibonachi_bubble_sort_bunary_search/num_Fibonachi_bubble_sort_bunary_search.cpp
@@ -3,21 +3,50 @@
 //Numbers Fibonachi
 
 // place in number 
-int Place_number_Fibonachi(int place_U,int stop_func = 0,int number1 = 0, int number2 = 1)
+int Place_number_Fibonachi(int place_U, int stop_func, int number1, int number2);
+//int Place_number_Fibonachi(int place_U, int stop_func = 0, int number1 = 0, int number2 = 1);
+// number in place 
+int Number_Fibonachi(int& place_U, const int number_stop_func, int number1, int number2);
+//int Number_Fibonachi(int& place_U, const int number_stop_func = 0, int number1 = 0, int number2 = 1);
+// User Fibonachi
+int User_Number_Fibonachi(int number_user1, int number_user2, int stop_func);
+//int User_Number_Fibonachi(int number_user1, int number_user2, int stop_func = 0);
+void INT_swaping(int &NUMBER1,int &NUMBER2);
+bool Uniqueness_number_array(int* point_array, int size, int number);
+void Fill_Dynamic_Array(int* point_array, int size);
+void Print_Dynamic_Array(int* point_array, int size);
+void Bubble_sort(int* point_array, int size);
+int main()
+{
+	srand(time(NULL));
+	int place{ 0 };
+	int number_user{0};
+	int number{0};
+	int size{ 10 };
+	int* Point_Array{ new int[size] };
+	Fill_Dynamic_Array(Point_Array, size);
+	Print_Dynamic_Array(Point_Array, size);
+	std::cout << "\tBye!" << std::endl;
+	delete[] Point_Array;
+	Point_Array = nullptr;
+	return 0;
+}
+
+int Place_number_Fibonachi(int place_U, int stop_func = 0, int number1 = 0, int number2 = 1)
 {
 	// place user
 	if (place_U == 1)
 	{
 		return 0;
 	}
-	if(place_U == 2||place_U==3)
+	if (place_U == 2 || place_U == 3)
 	{
 		return 1;
 	}
 	int buffer{ number1 + number2 };
 	stop_func++;
-	if(stop_func>place_U)
-	{ 
+	if (stop_func > place_U)
+	{
 		return -1;
 	}
 	if (stop_func == place_U)
@@ -25,17 +54,16 @@ int Place_number_Fibonachi(int place_U,int stop_func = 0,int number1 = 0, int nu
 		return buffer;
 	}
 	number1 = number2;
-	return Place_number_Fibonachi(place_U,stop_func,number1,buffer);
+	return Place_number_Fibonachi(place_U, stop_func, number1, buffer);
 }
-// number in place 
-int Number_Fibonachi(int& place_U, const int number_stop_func=0, int number1 = 0, int number2 = 1)
+int Number_Fibonachi(int& place_U, const int number_stop_func = 0, int number1 = 0, int number2 = 1)
 {
-	if(number_stop_func==0)
+	if (number_stop_func == 0)
 	{
 		place_U = 1;
 		return place_U;
 	}
-	if(number_stop_func==1)
+	if (number_stop_func == 1)
 	{
 		place_U = 2;
 		return place_U;
@@ -53,8 +81,7 @@ int Number_Fibonachi(int& place_U, const int number_stop_func=0, int number1 = 0
 	number1 = number2;
 	return Number_Fibonachi(place_U, number_stop_func, number1, buffer);
 }
-// User Fibonachi
-int User_Number_Fibonachi( int number_user1, int number_user2,int stop_func = 0)
+int User_Number_Fibonachi(int number_user1, int number_user2, int stop_func = 0)
 {
 	int buffer = { number_user1 + number_user2 };
 	if (stop_func == 0)
@@ -63,68 +90,111 @@ int User_Number_Fibonachi( int number_user1, int number_user2,int stop_func = 0)
 	}
 	number_user1 = number_user2;
 	stop_func--;
-	return User_Number_Fibonachi(number_user1, buffer,stop_func);
+	return User_Number_Fibonachi(number_user1, buffer, stop_func);
 }
-int main()
+void INT_swaping(int &num1,int &num2)
 {
-	int place{ 0 };
-	int number_user{0};
-	int number{0};
-	std::cout << "Enter number: " << std::endl;
-	for (int i = 0; i < 10; i++)
+	int buffer{ 0 };
+	buffer = num1;
+	num1 = num2;
+	num2 = buffer;
+}
+bool Uniqueness_number_array(int* point_array, int size, int number)
+{
+	bool uniqueness_num{ false };
+	for (int i = 0; i < size; i++)
 	{
-		std::cout << User_Number_Fibonachi(180, 115,i) << std::endl;
-	}
-
-
-
-
-
-
-
-
-
-
-	
-	/*while(std::cin>>number_user)
-	{
-		if (number_user != 0 || number_user != 1)
+		if (point_array[i] == number)
 		{
-			place += 2;
-		}
-		number_user = Number_Fibonachi(place, number_user);
-		if (number_user == 0)
-		{
-			std::cout << "This number isn't range Number_Fibonachi" << std::endl;
-			place = 0;
-		}
-		else if (number_user == 2)
-		{
-			std::cout << "Place: 2-3" << std::endl;
-			place = 0;
+			uniqueness_num = true;
+			break;
 		}
 		else
 		{
-			std::cout << "Place: " << number_user << std::endl;
-			place = 0;
+			uniqueness_num = false;
 		}
-	}*/
-
-	/*while (std::cin >> place)
+	}
+	return uniqueness_num;
+}
+void Fill_Dynamic_Array(int *point_array,int size) 
+{
+	int num{ 0 };
+	int i{ 0 };
+	do
 	{
-		number = Place_number_Fibonachi(place);
-		if(number==-1)
+		num = rand() % 100 + 1;
+		if (Uniqueness_number_array(point_array, size, num) == false)
 		{
-			std::cout << "Error" << std::endl;
+			point_array[i] = num;
+			i++;
 		}
-		else 
-		{ 
-			std::cout << "Number: " << Place_number_Fibonachi(place) << std::endl; 
+	} while (i<size);
+}
+void Print_Dynamic_Array(int* point_array, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << point_array[i] << std::endl;
+	}
+}
+void Bubble_sort(int *point_array,int size) 
+{
+	int element1{0}, element2{0};
+	bool sort_end{false};
+	do
+	{
+		if(point_array[element1]) 
+		{
+		
 		}
-	}*/
-	
-	std::cout << "Bye!" << std::endl;
-	return 0;
+	} while (sort_end==false);
 }
 
 
+
+
+
+
+
+
+/*std::cout << "Enter number: " << std::endl;
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << User_Number_Fibonachi(180, 115,i) << std::endl;
+	}*/
+	/*while(std::cin>>number_user)
+		{
+			if (number_user != 0 || number_user != 1)
+			{
+				place += 2;
+			}
+			number_user = Number_Fibonachi(place, number_user);
+			if (number_user == 0)
+			{
+				std::cout << "This number isn't range Number_Fibonachi" << std::endl;
+				place = 0;
+			}
+			else if (number_user == 2)
+			{
+				std::cout << "Place: 2-3" << std::endl;
+				place = 0;
+			}
+			else
+			{
+				std::cout << "Place: " << number_user << std::endl;
+				place = 0;
+			}
+		}*/
+
+		/*while (std::cin >> place)
+		{
+			number = Place_number_Fibonachi(place);
+			if(number==-1)
+			{
+				std::cout << "Error" << std::endl;
+			}
+			else
+			{
+				std::cout << "Number: " << Place_number_Fibonachi(place) << std::endl;
+			}
+		}*/
